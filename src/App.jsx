57239fada8b1d7b897b955e2fed1daae4b6e83cc
Baz003 <226179469+B0028@users.noreply.github.com>
2026-06-09 19:55:50 -0300

    chore(shadcn): install closes #4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './styles/App.css'
 import {NavigationMenu} from './components/ui/navigation-menu'
+import {Button} from './components/ui/button'
 
 function App() {
 
   return (
     <>
       <NavigationMenu/>
+      <Button>fdsaf</Button>
     </>
   )
 }
